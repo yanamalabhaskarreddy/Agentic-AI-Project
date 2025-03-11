e533752806d62b9c317c06e42a4aa1a2e1bae01f
yanamalabhaskarreddy <ybhaskarreddy1988@gmail.com>
2025-03-11 19:55:07 +0530

    Added basic chatbot code

---
+++ app.py
@@ -0,0 +1,8 @@
+#Program execution will start
+
+
+from src.langgraphagenticai.main import load_langgraph_agenticai_app
+
+
+if __name__=="__main__":
+    load_langgraph_agenticai_app()
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+# to package the folder
